28249b2ab5119573db43428ea195c8aedb7dc490
Thaily Samaniego <thaily.samaniego@tia.com.ec>
2022-11-21 05:40:45 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -163,6 +163,47 @@ class Dev(Configuration):
         'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
         'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "filters": ["require_debug_false"],
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
+
+
 
 class Prod(Dev):
     DEBUG = False
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,18 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    post = Post.objects.filter(published_at__lte=timezone.now())
-
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    #logger.debug("Got %d posts", len(post))
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
+  
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
@@ -17,6 +24,10 @@ def index(request):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
+
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
