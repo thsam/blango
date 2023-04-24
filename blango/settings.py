9e63e9bcefdffafaeb6094b5dcf62c22f72271b9
Thaily Samaniego <thaily.samaniego@tia.com.ec>
2023-04-24 01:11:44 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -221,6 +221,16 @@ LOGGING = {
 }
 
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
+}
 
 
 class Prod(Dev):
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,14 +1,24 @@
 from rest_framework import generics
-
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from rest_framework import permissions
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user == obj.author
