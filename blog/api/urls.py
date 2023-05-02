429e10b00f04a3ba942306856762c66aed10f540
Thaily Samaniego <thaily.samaniego@tia.com.ec>
2023-05-02 04:12:25 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,13 +4,18 @@ from drf_yasg.views import get_schema_view
 import os
 
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
 
-from blog.api.views import PostList, PostDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 
 urlpatterns = [
+    #path("posts/", PostList.as_view(), name="api_post_list"),
+    #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+    #path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
@@ -36,4 +41,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,27 @@ from rest_framework import permissions
 from blog.models import Post
 from blango_auth.models import User
 from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from blog.api.serializers import (
+    PostSerializer,
+    UserSerializer,
+    PostDetailSerializer,
+    TagSerializer,
+)
+from blog.models import Post, Tag
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    @action(methods=["get"], detail=True, name="Posts with the Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+            tag.posts, many=True, context={"request": request}
+        )
+        return Response(post_serializer.data)
 
 
 class PostList(generics.ListCreateAPIView):
@@ -28,3 +49,13 @@ class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
             return True
 
         return request.user == obj.author
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+    queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
+    router.register("posts", PostViewSet)
+
